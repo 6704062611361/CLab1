61c68c0d925b407596c2c5aa68a0dd832d56e34e
6704062611361 <s6704062611361@email.kmutnb.ac.th>
2024-11-28 14:20:50 +0700

    gonna go fastt

--- lab1minmax.c
+++ lab1minmax.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	int num1=5;
+	int num1=7;
 	int num2=6;
 	if (num1>num2){
-		printf(" num1 > num2");	
+		printf("num1 greater than num2");	
 	}
-	if (num1<num2){
-		printf(" num1 < num2");	
+	else{
+		printf("num2 greater than num1");	
 	}
 }
