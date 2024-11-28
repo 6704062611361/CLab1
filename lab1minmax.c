7f93a4b72a5a921bf6d2c412579a94acc5b3e202
6704062611361 <s6704062611361@email.kmutnb.ac.th>
2024-11-28 14:58:20 +0700

    3

--- lab1minmax.c
+++ lab1minmax.c
@@ -2,11 +2,10 @@
 int main()
 {
 	int n1;
-	printf("n1=");
-	scanf("%d", &n1);
 	int n2;
-	printf("n2=");
-	scanf("%d", &n2);
+	printf("n1 n2\n");
+	scanf("%d" "%d", &n1, &n2);
+
 	if (n1>n2){
 		printf("num1 greater than num2 by ");
 		printf("%d",n1-n2);	
